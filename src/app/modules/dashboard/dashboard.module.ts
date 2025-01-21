70bc741d3d667a19de312a8c0dd5b2e664ffa009
Lihue Leiva <leivalihue@gmail.com>
2025-01-21 20:43:54 -0300

    ADD, dashboard

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ToolbarComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+
+  ],
+  exports:[
+    DashboardComponent,
+  ]
+})
+export class DashboardModule { }
